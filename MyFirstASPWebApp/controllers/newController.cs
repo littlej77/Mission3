74187e4622d4ccac466bba2412e1b1c7877a654c
littlej77 <jordantempest@gmail.com>
2022-01-18 14:46:41 -0700

    initial commit

---
+++ MyFirstASPWebApp/controllers/newController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstASPWebApp.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPWebApp.controllers
+{
+    public class newController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult gradecalc ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult gradecalc (gradecalcmodel model) 
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MyFirstASPWebApp/models/gradecalcmodel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPWebApp.models
+{
+    //variable? (each variable needs a setter and a getter
+    public class gradecalcmodel
+    {
+        [Range(0,100)]
+        public float Assignments { get; set; }
+        [Range(0, 100)]
+        public float GroupProject { get; set; }
+        [Range(0, 100)]
+        public float Quizzes { get; set; }
+        [Range(0, 100)]
+        public float Test { get; set; }
+        [Range(0, 100)]
+        public float Intex { get; set; }
+        // the {get; set;} is doing what the following lines would do
+
+        //setter
+        //public void setAssignments( float input)
+        //{
+          //  this.Assignments = input;
+        //}
+
+        //getter
+        //public float getAssignments()
+       // {
+        //    return this.Assignments;
+       // }
+    }
+}
